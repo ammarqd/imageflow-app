6b8727564e55ab94b439b83fb82fdd5fbe54aa00
ammarqd <ammarqd@outlook.com>
2026-06-06 17:11:32 +0100

    build: add postgres, redis services and project structure

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI inside Docker!"}
+
+@app.post("/upload")
+async def upload_images(files: List[UploadFile] = File(...)):
+    return {
+        "filename": files.filename,
+        "content_type": files.content_type
+    }
